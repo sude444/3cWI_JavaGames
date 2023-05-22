8dae6af69672b5bc1ee0f6cea6b645caa17e6caa
sude <sude.bayar@student.htldornbirn.at>
2023-05-22 11:59:55 +0200

    03-Aufgabe-1-Objects_Rectangle_verbessert

--- src/at/bsu/games/shapes/Rectangle.java
+++ src/at/bsu/games/shapes/Rectangle.java
@@ -1,40 +1,93 @@
 package at.bsu.games.shapes;
 
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-public class Rectangle
-{
+public class Rectangle {
     private float x;
     private float y;
-    private float sizeX;
-    private float sizeY;
     private float speed;
+    private float width;
+    private float length;
+    private boolean rightDirection;
+    private boolean leftDirection;
+    private boolean downDirection;
+    private boolean uppDirection;
 
-    public Rectangle(float x, float y, float sizeX, float sizeY, float speed)
+    public Rectangle(float x,float y,float speed, float width, float length)
     {
-        this.x = x;
-        this.y = y;
-        this.sizeX = sizeX;
-        this.sizeY = sizeY;
-        this.speed = speed;
+        this.x=x;
+        this.y=y;
+        this.speed=speed;
+        this.width=width;
+        this.length=length;
+    }
+    public void init(GameContainer gameContainer) throws SlickException {
     }
 
-    public void update(int delta)
-    {
-        this.x += (float)delta/(this.speed/10);
-        if(this.x > 800)
+    public void Rectupdate(int i) {
+
+        if (this.x <=550&& this.y<=150)
         {
-            this.x = -50;
+            rightDirection=true;
+        }
+        else{rightDirection=false;}
+
+        if (this.y<=400&& this.x>=550)
+        {
+            downDirection=true;
+        }
+        else{downDirection=false;}
+
+        if (this.x>=150&& this.y>=400)
+        {
+            leftDirection=true;
+        }
+        else{leftDirection=false;}
+
+        if(this.x<=150&& this.y>=150)
+        {
+            uppDirection=true;
+        }
+        else{uppDirection=false;}
+
+
+        if(rightDirection==true)
+        {
+            this.x += (float) i / this.speed * 10;
+        }
+        if(leftDirection==true)
+        {
+            this.x -= (float) i / this.speed * 10;
+        }
+
+        if(downDirection==true)
+        {
+            this.y += (float) i / this.speed * 10;
+        }
+        if(uppDirection==true)
+        {
+            this.y -= (float) i / this.speed * 10;
         }
     }
-
-    public void render(Graphics graphics)
-    {
-        graphics.drawRect(this.x, this.y, this.sizeX, this.sizeY);
+    public float getX() {
+        return x;
     }
 
-    public float getX()
-    {
-        return x;
+    public float getY() {
+        return y;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getLength() {
+        return length;
     }
 }
