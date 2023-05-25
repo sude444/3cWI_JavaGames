954bc1ca749405b05b8a9ae8523058bfe3d447ef
sude <sude.bayar@student.htldornbirn.at>
2023-05-25 08:11:07 +0200

    03-Aufgabe-1-Objects_Main-fertig

--- src/at/bsu/games/shapes/Main.java
+++ src/at/bsu/games/shapes/Main.java
@@ -1,41 +1,12 @@
 package at.bsu.games.shapes;
+import at.bsu.games.firstgame.Rectangles;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
 
-import at.bsu.games.shapes.Rectangle;
-import org.newdawn.slick.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-public class Main extends BasicGame{
-
-    public Main(String title)
-    {
-        super(title);
-    }
-
-    private List<Rectangle> rectangleList;
-
-    @Override
-    public void init(GameContainer gameContainer) throws SlickException
-    {
-
-    }
-
-    @Override
-    public void update(GameContainer gameContainer, int delta) throws SlickException {
-        {
-            new Rectangle(200, 300, 200, 500, 500);
-        }
-    }
-
-    @Override
-    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-    graphics.drawRect(200,400,300,200);
-    }
+public class Main {
     public static void main(String[] argv) {
         try {
-            AppGameContainer container = new AppGameContainer(new Main("main"));
+            AppGameContainer container = new AppGameContainer(new Shapes("Shapes"));
             container.setDisplayMode(800,600,false);
             container.start();
         } catch (SlickException e) {
